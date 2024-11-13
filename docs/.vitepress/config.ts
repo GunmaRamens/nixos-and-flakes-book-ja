8e62170afc7d5a287f32d571166dd2cf22c57414
rytst <r.saito0111@gmail.com>
2024-11-14 01:54:01 +0900

    update(ja): Translate config.ts (sidebar)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -566,102 +566,102 @@ function themeConfigJapanese() {
       // https://vitepress.dev/reference/default-theme-config
       nav: [
         { text: "トップ", link: "/ja" },
-        { text: "Preface", link: "/ja/preface.md" },
-        { text: "入門する", link: "/ja/introduction/index.md" },
+        { text: "まえがき", link: "/ja/preface.md" },
+        { text: "入門", link: "/ja/introduction/index.md" },
         { text: "ベストプラクティス", link: "/ja/best-practices/intro.md" },
       ],
 
       sidebar: [
         {
-          text: "Preface",
-          items: [{ text: "Preface", link: "/ja/preface.md" }],
+          text: "まえがき",
+          items: [{ text: "まえがき", link: "/ja/preface.md" }],
         },
         {
-          text: "Get Started",
+          text: "入門",
           items: [
-            { text: "Introduction", link: "/ja/introduction/index.md" },
+            { text: "入門", link: "/ja/introduction/index.md" },
             {
-              text: "Advantages and Disadvantages",
+              text: "メリットとデメリット",
               link: "/ja/introduction/advantages-and-disadvantages.md",
             },
             {
-              text: "Installation",
+              text: "インストール",
               link: "/ja/introduction/installation.md",
             },
           ],
         },
         {
           text: "Nix 言語",
-          items: [{ text: "Basics", link: "/ja/the-nix-language/index.md" }],
+          items: [{ text: "基本の'き'", link: "/ja/the-nix-language/index.md" }],
         },
         {
           text: "NixOS と Flakes",
           items: [
             {
-              text: "Get Started with NixOS",
+              text: "NixOS 入門",
               link: "/ja/nixos-with-flakes/get-started-with-nixos.md",
             },
             {
-              text: "Introduction to Flakes",
+              text: "Flakes 入門",
               link: "/ja/nixos-with-flakes/introduction-to-flakes.md",
             },
             {
-              text: "NixOS with Flakes Enabled",
+              text: "NixOS で Flakes を有効にする",
               link: "/ja/nixos-with-flakes/nixos-with-flakes-enabled.md",
             },
             {
-              text: "NixOS's flake.nix Explained",
+              text: "flake.nix について知る",
               link: "/ja/nixos-with-flakes/nixos-flake-configuration-explained.md",
             },
             {
-              text: "The combination ability of Flakes and Nixpkgs module system",
+              text: "Flakes と Nixpkgs モジュールシステムを組み合わせる",
               link: "/ja/nixos-with-flakes/nixos-flake-and-module-system.md",
             },
             {
-              text: "Getting Started with Home Manager",
+              text: "Home Manager 入門",
               link: "/ja/nixos-with-flakes/start-using-home-manager.md",
             },
             {
-              text: "Modularize the Configuration",
+              text: "設定のモジュール化",
               link: "/ja/nixos-with-flakes/modularize-the-configuration.md",
             },
             {
-              text: "Updating the System",
+              text: "システムのアップデート",
               link: "/ja/nixos-with-flakes/update-the-system.md",
             },
             {
-              text: "Downgrading or Upgrading Packages",
+              text: "パッケージのアップ(ダウン)グレード",
               link: "/ja/nixos-with-flakes/downgrade-or-upgrade-packages.md",
             },
             {
-              text: "Other useful Tips",
+              text: "その他のヒント",
               link: "/ja/nixos-with-flakes/other-useful-tips.md",
             },
           ],
         },
         {
-          text: "Nixpkgs's Advanced Usage",
+          text: "Nixpkgs の発展的な使い方",
           items: [
-            { text: "Introduction", link: "/ja/nixpkgs/intro.md" },
+            { text: "入門", link: "/ja/nixpkgs/intro.md" },
             { text: "callPackage", link: "/ja/nixpkgs/callpackage.md" },
-            { text: "Overriding", link: "/ja/nixpkgs/overriding.md" },
+            { text: "Override", link: "/ja/nixpkgs/overriding.md" },
             { text: "Overlays", link: "/ja/nixpkgs/overlays.md" },
             {
-              text: "Multiple Nixpkgs Instances",
+              text: "複数の Nixpkgs インスタンス",
               link: "/ja/nixpkgs/multiple-nixpkgs.md",
             },
           ],
         },
         {
-          text: "Nix Store & Binary Cache",
+          text: "Nix Store とバイナリキャッシュ",
           items: [
-            { text: "Introduction", link: "/nix-store/intro.md" },
+            { text: "入門", link: "/nix-store/intro.md" },
             {
-              text: "Add Binary Cache Servers",
+              text: "バイナリキャッシュサーバの追加",
               link: "/ja/nix-store/add-binary-cache-servers.md",
             },
             {
-              text: "Host Your Own Binary Cache Server",
+              text: "バイナリキャッシュサーバをホスティングする",
               link: "/ja/nix-store/host-your-own-binary-cache-server.md",
             },
           ],
@@ -669,38 +669,38 @@ function themeConfigJapanese() {
         {
           text: "ベストプラクティス",
           items: [
-            { text: "Introduction", link: "/best-practices/intro.md" },
+            { text: "入門", link: "/best-practices/intro.md" },
             {
-              text: "Run downloaded binaries on NixOS",
+              text: "ダウンロードしたバイナリを NixOS で実行する",
               link: "/ja/best-practices/run-downloaded-binaries-on-nixos.md",
             },
             {
-              text: "Simplify NixOS-related Commands",
+              text: "NixOS に関連するコマンドを簡潔にする",
               link: "/ja/best-practices/simplify-nixos-related-commands.md",
             },
             {
-              text: "Accelerating Dotfiles Debugging",
+              text: "Dotfiles のデバッグを効率化する",
               link: "/ja/best-practices/accelerating-dotfiles-debugging.md",
             },
             {
-              text: "Custom NIX_PATH and Flake Registry",
+              text: "NIX_PATH と Flake レジストリのカスタム",
               link: "/ja/best-practices/nix-path-and-flake-registry.md",
             },
             {
-              text: "Remote Deployment",
+              text: "リモートデプロイ",
               link: "/ja/best-practices/remote-deployment.md",
             },
             {
-              text: "Debugging Derivations and Nix Expressions",
+              text: "Derivations と Nix 式のデバッグ",
               link: "/ja/best-practices/debugging.md",
             },
           ],
         },
 
         {
-          text: "Other Usage of Flakes",
+          text: "Flakes のその他の使い方",
           items: [
-            { text: "Introduction", link: "/other-usage-of-flakes/intro.md" },
+            { text: "入門", link: "/other-usage-of-flakes/intro.md" },
             {
               text: "Flake Inputs",
               link: "/ja/other-usage-of-flakes/inputs.md",
@@ -710,28 +710,28 @@ function themeConfigJapanese() {
               link: "/ja/other-usage-of-flakes/outputs.md",
             },
             {
-              text: "The New CLI",
+              text: "最新版の CLI",
               link: "/ja/other-usage-of-flakes/the-new-cli.md",
             },
             {
-              text: "Module System & Custom Options",
+              text: "モジュールシステム & カスタムオプション",
               link: "/ja/other-usage-of-flakes/module-system.md",
             },
             {
-              text: "[WIP]Testing",
+              text: "[WIP]テスト",
               link: "/ja/other-usage-of-flakes/testing.md",
             },
           ],
         },
         {
-          text: "Dev Environments on NixOS",
+          text: "NixOS における開発環境",
           items: [
             {
-              text: "nix shell, nix develop & pkgs.runCommand",
+              text: "nix shell, nix develop と pkgs.runCommand",
               link: "/ja/development/intro.md",
             },
             {
-              text: "Dev Environments",
+              text: "開発環境",
               link: "/ja/development/dev-environments.md",
             },
             {
@@ -739,26 +739,26 @@ function themeConfigJapanese() {
               link: "/ja/development/packaging-101.md",
             },
             {
-              text: "Cross-platform Compilation",
+              text: "クロスプラットフォームコンパイル",
               link: "/ja/development/cross-platform-compilation.md",
             },
             {
-              text: "Distributed Building",
+              text: "分散ビルド",
               link: "/ja/development/distributed-building.md",
             },
             {
-              text: "[WIP]Kernel Development",
+              text: "[WIP]カーネル開発",
               link: "/ja/development/kernel-development.md",
             },
           ],
         },
         {
-          text: "Advanced Topics",
-          items: [{ text: "Advanced Topics", link: "/ja/advanced-topics/index.md" }],
+          text: "発展的な内容",
+          items: [{ text: "発展的な内容", link: "/ja/advanced-topics/index.md" }],
         },
         {
-          text: "Frequently Asked Questions",
-          items: [{ text: "Frequently Asked Questions", link: "/ja/faq/index.md" }],
+          text: "よくある質問",
+          items: [{ text: "よくある質問", link: "/ja/faq/index.md" }],
         },
       ],
     },
