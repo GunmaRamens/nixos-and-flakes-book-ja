a0e22aa8e58dd0d0f40fcf6ad2d29c2048752329
hayao <hayao@fascode.net>
2024-11-15 04:06:04 +0900

    chore: Setup Japanese fork deployment

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,49 +1,32 @@
+/*
+NixOS & Flakes Book 日本語翻訳プロジェクト
+この設定ファイルは日本語翻訳プロジェクト用に変更されています。
+マージの際にこのファイルを変更しないよう注意してください。
+*/
+
 import { defineConfig } from "vitepress"
 import { shared } from "./shared"
-import { en } from "./en"
-import { zh } from "./zh"
 import { ja } from "./ja"
 
 export default defineConfig({
   ...shared,
   title: "NixOS & Flakes Book",
-
   rewrites: {
-    "en/:rest*": ":rest*",
+    "ja/:rest*": ":rest*",
   },
+  srcExclude: ["en/**/*.md", "zh/**/*.md"],
   locales: {
     root: {
+      label: "日本語",
+      ...ja,
+    },
+    en: {
       label: "English",
-      ...en,
+      link: "https://nixos-and-flakes.thiscute.world/",
     },
     zh: {
       label: "简体中文",
-      ...zh,
+      link: "https://nixos-and-flakes.thiscute.world/zh/",
     },
   },
-
-  // For forks in other languages, here is an example of how to add a new locale:
-  // rewrites: {
-  //   "ja/:rest*": ":rest*",
-  // },
-  // // Exclude the original language's markdown files when build dist
-  // // NOTE: You can still preview the original language's pages in dev mode
-  // srcExclude: ['zh/**/*.md', 'en/**/*.md'],
-  // locales: {
-  //   // Your language's root configuration
-  //   root: {
-  //     label: 'Japanese',
-  //     ...ja,
-  //   },
-  //
-  //   // Languages maintained by the original author
-  //   en: { 
-  //     label: 'English',
-  //   link: "https://nixos-and-flakes.thiscute.world/",
-  //   },
-  //   zh: {
-  //     label: '简体中文', 
-  //     link: "https://nixos-and-flakes.thiscute.world/zh/",
-  //   },
-  // },
 })
